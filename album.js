441b20817409b9850fc0399ec5c170bd7844a660
Donato2en <94622985+Donato2en@users.noreply.github.com>
2021-11-19 09:00:10 -0500

    Add files via upload

---
+++ album.js
@@ -0,0 +1,23 @@
+var album = {
+	"name": "Nothing but the beat",
+	"artist": "David Guetta",
+	"cover": "cover.jpg",
+	"songList": [
+                { "name": "All The Way Up", "interpreter": "David Guetta / Fat Joe / Remy Ma", "duration": 233, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Chillin%20Hard.mp3"},
+		{ "name": "Club Can't Handle Me", "interpreter": "David Guetta / Flo Rida", "duration": 197, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Voice%20Over%20Under.mp3"},
+		{ "name": "She Wolf", "interpreter": "David Guetta / Sia", "duration": 278, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/What%20Is%20Love.mp3"},
+		{ "name": "Memories", "interpreter": "David Guetta / Kid Cudi", "duration": 233, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Chillin%20Hard.mp3"},
+		{ "name": "When Love Takes Over", "interpreter": "David Guetta / Kelly Rowland", "duration": 197, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Voice%20Over%20Under.mp3"},
+		{ "name": "Where Them Girls At", "interpreter": "David Guetta /  Flo Rida / Nicki Minaj", "duration": 278, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/What%20Is%20Love.mp3"},
+                { "name": "Titanium", "interpreter": "David Guetta / Sia", "duration": 233, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Chillin%20Hard.mp3"},
+		{ "name": "Little Bad Girl", "interpreter": "David Guetta / Taio Cruz / Ludacris", "duration": 197, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Voice%20Over%20Under.mp3"},
+                { "name": "Sweat", "interpreter": "David Guetta / Snoop Dogg", "duration": 278, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/What%20Is%20Love.mp3"},
+		{ "name": "Turn Me On", "interpreter": "David Guetta / Nicki Minaj", "duration": 233, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Chillin%20Hard.mp3"},
+		{ "name": "Without You", "interpreter": "David Guetta / Usher", "duration": 197, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Voice%20Over%20Under.mp3"},
+		{ "name": "Nothing Really Matters", "interpreter": "David Guetta / will.i.am", "duration": 278, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/What%20Is%20Love.mp3"},
+		{ "name": "I Can Only Imagine", "interpreter": "David Guetta / Chris Brown / Lil'Wayne", "duration": 233, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Chillin%20Hard.mp3"},
+		{ "name": "Crank It Up", "interpreter": "David Guetta / Akon", "duration": 197, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Voice%20Over%20Under.mp3"},
+		{ "name": "Repeat", "interpreter": "David Guetta / Jessie J", "duration": 278, "url": "http://incompetech.com/music/royalty-free/mp3-royaltyfree/What%20Is%20Love.mp3"}
+	]
+};
+
